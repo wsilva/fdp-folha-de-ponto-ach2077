f7dc6a64719a41f70b6b7f949a594dbdcdb51a91
Wellington Silva <wsilva@usp.br>
2014-12-17 07:53:43 -0200

    creating model, templates and url for pontos

--- fdp/urls.py
+++ fdp/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fdp.views.home', name='home'),
+    url(r'^$', 'pontos.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- pontos/models.py
+++ pontos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Registro(models.Model):
+
+    registro = models.DateTimeField(auto_now_add = False, auto_now=False)
+    criado_em = models.DateTimeField(auto_now_add = True, auto_now=False)
+    alterado_em = models.DateTimeField(auto_now_add = False, auto_now=True)
+    imagem = models.CharField(max_length=120, default='no-image.jpg')
+
+
+    def __unicode__(self):
+
+        return "%s" % self.registro
