1ba6b8c8acc4b3b96ef7ed366fe4e73196f2434d
Wellington Silva <wsilva@usp.br>
2014-12-17 07:54:26 -0200

    first unit test

--- pontos/tests.py
+++ pontos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from pontos.models import Registro
+
+class RegistroTests(TestCase):
+
+    def test_str(self):
+
+        registro = Registro(registro='2014-12-16 03:12',
+                            criado_em='2014-12-17 06:26:54',
+                            alterado_em='2014-12-17 06:26:54')
+        print registro
+
+        self.assertEquals(
+            str(registro),
+            '2014-12-16 03:12',
+        )
