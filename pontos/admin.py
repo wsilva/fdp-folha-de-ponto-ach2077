0939655291c3b7d1b482625079889484eb16df44
Wellington Silva <wsilva@usp.br>
2014-12-29 21:27:46 -0200

    Configuring Registro model inside admin backend

--- pontos/admin.py
+++ pontos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Registro
+
+class RegistroAdmin(admin.ModelAdmin):
+    list_display=['user', 'registro', 'criado_em', 'alterado_em']
+    class Meta:
+        model = Registro
+
+admin.site.register(Registro, RegistroAdmin)
