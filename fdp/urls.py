04e18cfbc3e4670b6e7df1d32561baed155e3e9b
Wellington Silva <wsilva@usp.br>
2014-12-29 21:28:41 -0200

    setting up urls to login, register, and insert datasheet entries

--- fdp/urls.py
+++ fdp/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fdp.views.home', name='home'),
     url(r'^$', 'pontos.views.home', name='home'),
+    url(r'^novo-ponto$', 'pontos.views.novoponto', name='novoponto'),
+    url(r'^novo$', 'pontos.views.novo', name='novo'),
+    url(r'^registration/$', 'pontos.views.registration', name='registration'),
+    url(r'^login/$', 'pontos.views.user_login', name='login'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
