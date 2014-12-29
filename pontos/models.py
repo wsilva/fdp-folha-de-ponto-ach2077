b8ae4bfd7f062b021a37c964f2dd622ddf4d5ecf
Wellington Silva <wsilva@usp.br>
2014-12-29 20:17:49 -0200

    changing class registro to link with an user

--- pontos/models.py
+++ pontos/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 class Registro(models.Model):
 
+    user = models.ForeignKey('auth.user')
     registro = models.DateTimeField(auto_now_add = False, auto_now=False)
+    # comprovante = models.FileField(upload_to=UPLOAD_DIR, max_length=100)
     criado_em = models.DateTimeField(auto_now_add = True, auto_now=False)
     alterado_em = models.DateTimeField(auto_now_add = False, auto_now=True)
-    imagem = models.CharField(max_length=120, default='no-image.jpg')
 
+    def save(self, *args, **kwargs):
+        super(Registro, self).save(*args, **kwargs)
 
     def __unicode__(self):
 
